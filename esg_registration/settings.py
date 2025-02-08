5fc63717289df994e44ba6634ee365e1db1c0dfb
Kizito Daka <kizitodak@gmail.com>
2025-02-08 11:17:29 +0100

    Updated settings.py

--- esg_registration/settings.py
+++ esg_registration/settings.py
@@ -154,10 +154,3 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.hostinger.com'
-EMAIL_PORT = 465
-# EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'info@esgforumafrica.com'
-EMAIL_HOST_PASSWORD = '@Esgforum2023@'
